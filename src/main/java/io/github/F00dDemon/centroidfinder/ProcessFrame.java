83b7012f63961c6bfa70cfb0ef2041526b3c3f11
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2026-05-22 18:08:44 -0700

    Added fallback for null or empty frames

--- src/main/java/io/github/F00dDemon/centroidfinder/ProcessFrame.java
+++ src/main/java/io/github/F00dDemon/centroidfinder/ProcessFrame.java
@@ -4,13 +4,19 @@ import java.awt.image.BufferedImage;
 import java.util.List;
 
 public class ProcessFrame {
-    public Group ProcessSingleFrame(BufferedImage frame, int targetColor, int threshold, int second) {
-        ColorDistanceFinder distanceFinder = new EuclideanColorDistance();
-        ImageBinarizer binarizer = new DistanceImageBinarizer(distanceFinder, targetColor, threshold);
+    public Group ProcessSingleFrame(BufferedImage frame, int targetColor, int threshold) {
+        try {
+            ColorDistanceFinder distanceFinder = new EuclideanColorDistance();
+            ImageBinarizer binarizer = new DistanceImageBinarizer(distanceFinder, targetColor, threshold);
         
-        ImageGroupFinder groupFinder = new BinarizingImageGroupFinder(binarizer, new DfsBinaryGroupFinder());
+            ImageGroupFinder groupFinder = new BinarizingImageGroupFinder(binarizer, new DfsBinaryGroupFinder());
         
-        List<Group> groups = groupFinder.findConnectedGroups(frame);
-        return groups.get(0);
+            List<Group> groups = groupFinder.findConnectedGroups(frame);
+
+            if (groups == null || groups.isEmpty()) return new Group(0, new Coordinate(-1, -1)); 
+
+            return groups.get(0);
+        } 
+        catch (Exception e) { return new Group(0, new Coordinate(-1, -1)); }
     }
 }
