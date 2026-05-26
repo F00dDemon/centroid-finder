8684f01bddfed1a162dbe41f4a4f95b2d39c9642
f00d <delossantos.orion@student.greenriver.edu>
2026-05-26 00:12:39 -0700

    making extractVideoData class

--- src/main/java/io/github/F00dDemon/centroidfinder/ExtractVideoData.java
+++ src/main/java/io/github/F00dDemon/centroidfinder/ExtractVideoData.java
@@ -1,5 +1,35 @@
 package io.github.F00dDemon.centroidfinder;
 
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+
 public class ExtractVideoData {
-    public static 
+        public int width;
+        public int height;
+        public double frameRate;
+        public long frameCount;
+
+        @Override
+        public String toString() {
+            return "VideoMetadata{" +
+                    "width=" + width +
+                    ", height=" + height +
+                    ", frameRate=" + frameRate +
+                    ", frameCount=" + frameCount;
+        }
+        public void extractMetadata(String videoPath) throws Exception {
+        try (FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(videoPath)) {
+            try {
+                grabber.start();
+
+                this.width = grabber.getImageWidth();
+                this.height = grabber.getImageHeight();
+                this.frameRate = grabber.getFrameRate();
+                this.frameCount = grabber.getLengthInFrames();
+
+            } finally {
+                grabber.stop();
+                grabber.release();
+            }
+        }
+    }
 }
--- src/main/java/io/github/F00dDemon/centroidfinder/VideoSummaryApp.java
+++ src/main/java/io/github/F00dDemon/centroidfinder/VideoSummaryApp.java
@@ -10,4 +10,30 @@ public class VideoSummaryApp {
     public long durationMicros;
     public String videoCodec;
     public String format;
+
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java VideoSummaryApp <input_video> <hex_target_color> <threshold>");
+            return;
+        }
+
+        String inputVideoPath = args[0];
+        String hexTargetColor = args[1];
+        int threshold = 0;
+        try {
+            threshold = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Threshold must be an integer.");
+            return;
+        }
+
+        ExtractVideoData metaData = new ExtractVideoData();
+        try {
+            metaData.extractMetadata(inputVideoPath);
+        } catch (Exception e) {
+            System.err.println(e);
+            return;
+        };
+
+    }
 }
